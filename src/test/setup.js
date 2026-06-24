beb3f20ff4e4e81082ab3d33301007080f3d608b
최재관 <onlyvideo925@gmail.com>
2026-06-25 03:34:11 +0900

    feat(ui): profile store with localStorage persistence

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+
+// Ensure localStorage is available in Node environment for zustand persist
+if (typeof global.localStorage === 'undefined') {
+  global.localStorage = {
+    getItem: () => null,
+    setItem: () => {},
+    removeItem: () => {},
+    clear: () => {},
+  }
+}
